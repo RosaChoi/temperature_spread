1ff7259ce3fa278bf2319f7526d979cb8956a487
waselrosa <rosa@waselrosa.com>
2014-11-06 18:14:11 -0800

    used dot sort_by method to refactor my previous code on ClassDataAnalysis

--- lib/class_data_analysis.rb
+++ lib/class_data_analysis.rb
@@ -21,21 +21,31 @@ class ClassDataAnalysis
   end
 
   def day_of_lowest_temperature
-    array_of_low = []
+    # array_of_low = []
+    #
+    # data.each do |instance|
+    #   array_of_low << instance.low
+    # end
+    # array_of_low.index(array_of_low.min)+1
 
-    data.each do |instance|
-      array_of_low << instance.low
-    end
-    array_of_low.index(array_of_low.min)+1
+    # Found out about the method .sort_by and was able to refactor the above code
+
+    data.sort_by do |i|
+      (i.low)
+    end.first.day
   end
 
   def day_of_highest_temperature
-    array_of_high = []
+    # array_of_high = []
+    #
+    # data.each do |instance|
+    #   array_of_high << instance.high
+    # end
+    # array_of_high.index(array_of_high.max)+1
 
-    data.each do |instance|
-      array_of_high << instance.high
-    end
-    array_of_high.index(array_of_high.max)+1
+    data.sort_by do |i|
+      p i.high
+    end.last.day
   end
 
   def average_temperatures
